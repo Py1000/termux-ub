6b47e43574f66ca21c4ff6729b1ee73fbf50a1ad
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-03-04 10:35:53 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 import os
-from telethon import TelegramClient
+from telethon import TelegramClient,events
 from telethon.sessions import StringSession
 
 e = "Lul Error"
@@ -17,8 +17,12 @@ SESSION = input("Enter SESSION: ")
 session = str(SESSION)
 
 user = TelegramClient(StringSession(session), API_ID, API_HASH)
+user.start()
 
 async def startBot():
   await user.send_message("me","Hello!")
-async user.loop.run_until_complete(startBot)
+
+@user.on(events.NewMessage(pattern=".hi"))
+async def hi(event):
+  await evemt.edit("Hello there!")
 user.run_until_disconnected()
