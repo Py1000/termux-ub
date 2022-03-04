3a1300f65d4da921fa4942de01067d64fc16c61a
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-03-04 16:36:24 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -65,6 +65,6 @@ async def alive(event):
 
 @user.on(events.NewMessage(pattern="\."))
 async def addons(event):
-  rawTxt = event.raw_text()
+  rawTxt = event.raw_text.split()
 
 user.run_until_disconnected()
