73db08cb44d9cfbf8472649a0992d63d976dd18b
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-03-05 19:59:52 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -104,7 +104,7 @@ user = TelegramClient(StringSession(session), API_ID, API_HASH)
 user.start()
 os.system("clear")
 print("BOT STARTUP COMPLETE \nDo .hi in any chat \n《---BOT LOG---》")
-print(ALIVE_PIC)
+print(user.id)
 
 
 async def startBot():
